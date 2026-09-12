144d21afb85ceb5a516787eb1c8e8169823a0710
AvisekhB <73953623+AvisekhB@users.noreply.github.com>
2026-09-12 16:30:07 +0530

    c
    ac

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+// GitHub Pages project-site URL:
+// https://avisekhb.github.io/Nityaseva-React-app/
+export default defineConfig({
+  base: '/Nityaseva-React-app/',
+});
